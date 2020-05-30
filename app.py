ef50d9053b2edd0c447652035c0663808cf007d5
naranundra <64673333+naranundra@users.noreply.github.com>
2020-05-30 11:13:25 +0800

    Программ

---
+++ app.py
@@ -0,0 +1,18 @@
+from function import showMenu, create, remove
+
+showMenu()
+
+n = int(input('Үйдлийн дугаар оруулна уу: '))
+
+if n == 1:
+    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
+elif n == 2:
+    create()
+elif n == 3:
+    remove()
+elif n == 4:
+    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
+else:
+    print('Та программаас гарлаа...')
+
+
